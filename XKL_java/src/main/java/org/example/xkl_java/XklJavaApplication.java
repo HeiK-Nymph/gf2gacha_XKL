0c8055d57782cca74cf0c7e9a73d3ed3018ffe79
HeiK-Nymph <2353611438@qq.com>
2026-04-06 17:52:05 +0800

    加快了特别多查询速度

---
+++ XKL_java/src/main/java/org/example/xkl_java/XklJavaApplication.java
@@ -0,0 +1,13 @@
+package org.example.xkl_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XklJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XklJavaApplication.class, args);
+    }
+
+}
---
+++ XKL_java/src/test/java/org/example/xkl_java/XklJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.xkl_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XklJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
